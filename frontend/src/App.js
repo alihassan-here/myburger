109d7ea8d0fda50a85fefbac1cd8fd95539d546b
alihassa-here <alihassanuog50@gmail.com>
2022-09-24 23:17:36 +0500

    order ui completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import "./styles/menu.scss";
 import "./styles/footer.scss";
 import "./styles/contact.scss";
 import "./styles/cart.scss";
+import "./styles/shipping.scss";
+import "./styles/confirmOrder.scss";
+import "./styles/paymentSuccess.scss";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
@@ -13,6 +16,9 @@ import Home from "./components/home/Home";
 import Footer from "./components/layout/Footer";
 import Contact from "./components/contact/Contact";
 import Cart from "./components/cart/Cart";
+import Shipping from "./components/cart/Shipping";
+import ConfirmOrder from "./components/cart/ConfirmOrder";
+import PaymentSuccess from "./components/cart/PaymentSuccess";
 
 function App() {
   return (
@@ -22,6 +28,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/confirmorder" element={<ConfirmOrder />} />
+        <Route path="/paymentsuccess" element={<PaymentSuccess />} />
       </Routes>
       <Footer />
     </Router>
