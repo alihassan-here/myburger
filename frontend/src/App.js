899d371f2d5221a5408374fed26479bd51f0a534
alihassa-here <alihassanuog50@gmail.com>
2022-09-25 23:45:14 +0500

    login with dashboard ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,8 @@ import "./styles/cart.scss";
 import "./styles/shipping.scss";
 import "./styles/confirmOrder.scss";
 import "./styles/paymentSuccess.scss";
+import "./styles/login.scss";
+import "./styles/profile.scss";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
@@ -19,11 +21,13 @@ import Cart from "./components/cart/Cart";
 import Shipping from "./components/cart/Shipping";
 import ConfirmOrder from "./components/cart/ConfirmOrder";
 import PaymentSuccess from "./components/cart/PaymentSuccess";
+import Login from "./components/login/Login";
+import Profile from "./components/profile/Profile.jsx";
 
 function App() {
   return (
     <Router>
-      <Header />
+      <Header isAuthenticated={true} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
@@ -31,6 +35,8 @@ function App() {
         <Route path="/shipping" element={<Shipping />} />
         <Route path="/confirmorder" element={<ConfirmOrder />} />
         <Route path="/paymentsuccess" element={<PaymentSuccess />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/me" element={<Profile />} />
       </Routes>
       <Footer />
     </Router>
